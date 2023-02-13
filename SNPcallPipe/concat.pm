30d66e4b52fb4000b9e78dbdd128ec80e367921d
Yuma248 <yuma248@gmail.com>
2023-02-13 17:47:06 +1030

    Create concat.pm

---
+++ SNPcallPipe/concat.pm
@@ -0,0 +1,47 @@
+package coca;
+
+my $LEVEL = 1;
+sub concat{
+my @arg = @_;
+foreach $ar (@arg){
+	if ($ar =~ /^-i/){our $inputfolder = (split(/ /,$ar))[1];}
+	elsif($ar=~ /^-o/){our $outputfolder = (split(/ /, $ar))[1];}
+	elsif ($ar=~ /^-snc/){$snc=(split(/ /, $ar))[1];}
+	elsif ($ar=~ /^-t/) {$type = (split(/ /, $ar))[1];}
+	elsif ($ar=~ /^-exf/) {$exf = (split(/ /, $ar))[1];}
+}
+use File::Path qw( make_path );
+if (not defined ($inputfolder && $outputfolder)){print "\nThis script will concatenate fastq files of raw or trimmed reads of several samples in parallel. It requires fastq files of all samples stored in the same folder\n\nUsage:\nConcat.pl\n\t-i <path to the folder with the input fastq files>\n\t-o <path to the output folder>\n Optional:\n\t-snc <number of runs in parallel, default 10>\n\t-t <method used for trimming. Trimmomatic TR, AdapterRemoval AR or None NO if are raw sequences, default AR>\n\t-exf <this will tell the script how to extract the name of each sample, and should include all extra information at the end of the file names that is not related to the sample name, default P1_L001_>\n\nFor example:\nconcat.pl -i /home/Yumafan/demultiplex/trimmed/ -o /home/Yumafan/concatenated-snc 10 -t AR -exf P1_L001_\n\n"; exit;}
+
+
+if ( !-d $outputfolder ) {
+	make_path $outputfolder or die "Failed to create path: $outputfolder";
+}
+our $tmpdir=$outputfolder."/tmp";
+if ( !-d $tmpdir ) {
+        `mkdir $tmpdir`;
+}
+if (not defined ($snc)){$snc =10;}
+
+if ($type == "AR"){
+	my @names = `ls $inputfolder/*$exf\.collapsed.gz`;
+	foreach $name (@names) {chomp $name; $name=~ s/$inputfolder\//g; $name = s/$exf\.collapsed\.gz//g; push (@nms, $name);}
+	foreach $ef ( "singleton.truncated", "collapsed", "collapsed.truncated", "pair1.truncated", "pair2.truncated"){
+		my $cmd = "parallel -j $snc --results $tmpdir --tmp $tmpdir zcat $inputfolder\/{1}*\.$ef\.gz | gzip \'>>\' $outputfolder/{1}\.$ef\.gz ::: @nms";
+		print "$cmd\n";
+		`parallel -j $snc --results $tmpdir --tmp $tmpdir zcat $inputfolder\/{1}*\.$ef\.gz \'|\' gzip \'>\'\'>\' $outputfolder/{1}\.$ef\.gz ::: @nms`
+		}
+}
+elsif ($type == "TR" or $type == "NO"){
+	my @names = `ls $inputfolder/*$exf\.gz`;
+	foreach $name (@names) {chomp $name; $name=~ s/$inputfolder\//g; $name = s/$exf\.gz//g; push (@nms, $name);}
+	my $cmd = "parallel -j $snc --results $tmpdir --tmp $tmpdir zcat $inputfolder\/{1}*\.$ef\.gz | gzip \'>>\' $outputfolder/{1}\.$ef\.gz ::: @nms";
+	print "$cmd\n";
+	`parallel -j $snc --results $tmpdir --tmp $tmpdir zcat $inputfolder\/{1}*\.gz \'|\' gzip \'>\'\'>\' $outputfolder/{1}\.gz @nms ::: @nms`
+}
+else {print "You have to select a correct type of file, check the instrcution\n"}
+
+
+} 
+
+1;
