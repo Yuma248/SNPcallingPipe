7f0578cc5daefd52f71f6139f5328b4dc623e7fa
Yuma248 <yuma248@gmail.com>
2023-02-15 17:10:38 +1030

    Update indelrealigment.pm

--- SNPcallPipe/indelrealigment.pm
+++ SNPcallPipe/indelrealigment.pm
@@ -12,19 +12,10 @@ foreach $ar (@arg){
         elsif ($ar=~ /^-ind /){our $ind=(split(/ /,$ar))[1];}
         elsif ($ar=~ /^-ncp /){our $ncp=(split(/ /,$ar))[1];}
 	elsif ($ar=~ /^-rg /){$refgenome=(split(/ /,$ar))[1];}
+	elsif ($ar=~ /^-lnc /){$lnc=(split(/ /,$ar))[1];}
+	
 }
-#while (@ARGV){                                          
-#        $_=shift @ARGV;                                 
-#        if ($_=~ /^-rg$/){$refgenome=shift @ARGV;}       
-#        elsif($_=~ /^-i$/){$inputfolder=shift @ARGV;}   
-#        elsif ($_=~ /^-o$/){$outputfolder=shift @ARGV;} 
-#        elsif ($_=~ /^-ncp$/){$ncp=shift @ARGV;}     
-#	elsif ($_=~ /^-ind$/){$ind=shift @ARGV;}
-#my $inputfolder = $ARGV[0];
-#my $outputfolder = $ARGV[1];
-#my $refgenome = $ARGV[2];
-#my $ncores =$ARGV[3];
-#}
+
 if (not defined ($refgenome && $inputfolder && $outputfolder)){print "\nThis script will create a list of indel per sample, and it will realign reads around indels for a more efficient variants calling. It uses GATK in parallel to process several samples at the same time.\n\nUsage:\nIndelRealigment\n\t-i <input folder with sorted bam files>\n\t-o <output folder to save the realigments>\n\t-rg <path to the reference genome>\nOptional:\n\t-ncp <number cores to run in parallel, default 4>\n\t-ind <\"yes\" if you need to index your bam files, or \"no\" if they are already indexed, default \"yes\">\n\nFor example: IndelRealigment -i ./Yuma/sam2bamout/ -o ./Yuma/indelout/ -rg ./Yuma/genomes/reference.fasta -ncp 8 -ind yes\n\n"; exit;}
 opendir(DIR, "$inputfolder") or die "Can not open inputfolder $inputfolder, $!\n" ;
 my @files = readdir(DIR);
@@ -44,13 +35,13 @@ foreach my $file (@files){next unless ($file =~ /\.bam$/ ); my @fileinf =split (
 
 
 if (@pops){
-$cmd2="parallel --xapply -j 38 --results ./logfilesIR --noswap samtools index $inputfolder\/{2}_{1}_{3}\.bam ::: @samplesnames ::: @pops ::: @term";
+$cmd2="parallel --xapply -j $lnc --results ./logfilesIR --noswap samtools index $inputfolder\/{2}_{1}_{3}\.bam ::: @samplesnames ::: @pops ::: @term";
 $cmd1="parallel --xapply -j $ncp --results ./logfilesIR --noswap java -jar ./SNPcallPipe/GenomeAnalysisTK.jar -T RealignerTargetCreator -R $refgenome -I $inputfolder\/{2}\_{1}_{3}\.bam -o $outputfolder\/{2}\_{1}\_intervals\.list ::: @samplesnames ::: @pops ::: @term";
 $cmd="parallel --xapply -j $ncp --results ./logfilesIR --noswap java -jar ./SNPcallPipe/GenomeAnalysisTK.jar -T IndelRealigner -R $refgenome -I $inputfolder\/{2}\_{1}_{3}\.bam -targetIntervals $outputfolder\/{2}\_{1}\_intervals\.list -o $outputfolder\/{2}\_{1}\_realigned.bam ::: @samplesnames ::: @pops ::: @term";
 
 }
 else{
-$cmd2="parallel -j 38 --results ./logfilesIR --noswap samtools index $inputfolder\/{1}_{2}\.bam ::: @samplesnames ::: @term";
+$cmd2="parallel -j $lnc --results ./logfilesIR --noswap samtools index $inputfolder\/{1}_{2}\.bam ::: @samplesnames ::: @term";
 #$cmd3="parallel -j $ncp --results ./logfilesIR --noswap samtools index $inputfolder\/sort_{1}\.bam ::: @samplesnames";
 $cmd1 ="parallel -j $ncp --results ./logfilesIR --noswap java -jar ./SNPcallPipe/GenomeAnalysisTK.jar -T RealignerTargetCreator -R $refgenome -I $inputfolder\/{1}_{2}\.bam -o $outputfolder\/{1}\_intervals\.list ::: @samplesnames ::: @term";
 $cmd ="parallel -j $ncp --results ./logfilesIR --noswap java -jar ./SNPcallPipe/GenomeAnalysisTK.jar -T IndelRealigner -R $refgenome -I $inputfolder\/{1}_{2}\.bam -targetIntervals $outputfolder\/{1}\_intervals\.list -o $outputfolder\/{1}\_realigned.bam ::: @samplesnames ::: @term";
