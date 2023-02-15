984a0d0a76d74571533fb29a1a389abdb78d8bab
Yuma248 <yuma248@gmail.com>
2023-02-15 14:57:20 +1030

    Update samdedup.pm

--- SNPcallPipe/samdedup.pm
+++ SNPcallPipe/samdedup.pm
@@ -9,13 +9,7 @@ foreach $ar (@arg){
 	elsif ($ar=~ /^-nc/){our $nc=(split(/ /,$ar))[1];}
 	elsif ($ar=~ /^-ncp/){our $ncp=(split(/ /,$ar))[1];}
 }
-#while (@ARGV){
-#$_=shift @ARGV;
-#if ($_=~ /^-i$/){$inf=shift @ARGV;}
-#elsif ($_=~ /^-o$/){$outf=shift @ARGV;}
-#elsif ($_=~ /^-nc$/){$nc=shift @ARGV;}
-#elsif ($_=~ /^-ncp$/){$ncp=shift @ARGV;}
-#}
+
 if (not defined ($inf)){print "\nThis script will convert from sam to bam, sort by name, fixmates, sort by coordinates and markduplicates using samtools in parallel. Requires your sam files after mapping (sample01.sam), all save in one folder.\n\nUsages: samtoolsdup.pl\n\t-i <path to inputfolder>\n\nOptional:\n\t-o <path to outputfolder, default samout>\n\t-nc <number the cores or samples to use in parallel, default 4>\n\t-ncp <number of cores per sample, default 1>\n\n";exit;}
 if (not defined ($outf)){$outf="./samout";}
 our $tmpdir=$outf."/tmp";
@@ -43,13 +37,9 @@ my $cmd3="parallel -j $nc samtools sort -\@ $ncp -o $coor/{1}_positionsort.bam $
 my $cmd4="parallel -j $nc samtools markdup -r -s $coor/{1}_positionsort.bam $ddp/{1}_markdup.bam \'\>\' $tmpdir/{1}_log ::: @nms";
 print "$cmd\n\n$cmd2\n\n$cmd3\n\n$cmd4\n\n";
 `parallel -j $nc samtools view -bS $inf/{1}.sam \'\|\' samtools sort -n -\@ $ncp -o $names/{1}_namesort.bam - ::: @nms`;
-#`parallel -j $nct mv $inf/{1}.sam /scratch/user/reev0074/merged/ ::: @nms`;
 `parallel -j $nc samtools fixmate -m $names/{1}_namesort.bam $fix/{1}_fixmate.bam ::: @nms`;
-#`parallel -j $nct mv $names/{1}_namesort.bam /scratch/user/reev0074/merged/ ::: @nms`;
 `parallel -j $nc samtools sort -\@ $ncp -o $coor/{1}_positionsort.bam $fix/{1}_fixmate.bam ::: @nms`;
-#`parallel -j $nct mv $fix/{1}_fixmate.bam /scratch/user/reev0074/merged/ ::: @nms`;
 `parallel -j $nc --results $tmpdir samtools markdup -r -s $coor/{1}_positionsort.bam $ddp/{1}_markdup.bam ::: @nms`; 
-#`parallel -j $nct mv $coor/{1}_positionsort.bam /scratch/user/reev0074/merged/ ::: @nms`;
 
 }
 
