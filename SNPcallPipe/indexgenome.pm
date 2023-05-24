27ef8bb24a4c4e2aaaf54adf198bc79b2cc2a7d8
Yuma <yuma248@gmail.com>
2023-05-24 16:49:40 +0930

    Update indexgenome.pm

--- SNPcallPipe/indexgenome.pm
+++ SNPcallPipe/indexgenome.pm
@@ -8,7 +8,7 @@ foreach $ar (@arg){
         elsif ($ar=~ /^-rg/){$ref=(split(/ /,$ar))[1];}
 	elsif ($ar=~ /^-pf/){$pf=(split(/ /,$ar))[1];}
 }
-if (not defined ($inputfolder && $ref)){print "\nThis script will indexs a reference genome using samtools, picard and bowtie2. The genome should have extention fna.\n\nUsage:\n\t-i <input folder where the reference is, and where all the indeces and dictionaries will be saved>\n\t-rg <the name of the reference genome incluiding the extention>\n\t-pf <path to the picar jar executable>\n\nExample:SNPcallpipe.pl -stp indref -i yume/genomes/ -rg Taust.Chr.fna -pf /local/SNOcallPipe/\n"; exit;}
+if (not defined ($inputfolder && $ref)){print "\nThis script will indexs a reference genome using samtools, picard, bowtie2 and snap. The genome should have extention fna.\n\nUsage:\n\t-i <input folder where the reference is, and where all the indeces and dictionaries will be saved>\n\t-rg <the name of the reference genome incluiding the extention>\n\t-pf <path to the picar jar executable>\n\nExample:SNPcallpipe.pl -stp indref -i yume/genomes/ -rg Taust.Chr.fna -pf /local/SNOcallPipe/\n"; exit;}
 
 $ref =~ s/$inputfolder//;
 our $orgn = $ref;
@@ -18,12 +18,13 @@ unless ($ref =~ m/\.fna$/){
 	`mv $inputfolder\/$ref $inputfolder\/$orgn\.fna`; 
 }
 $orgn =~ s/\.fna$//;
-
+$OUTSNAP ="$inputfolder\/$orgn\_SNAP";
+`mkdir $OUTSNAP`;
+	
 `samtools faidx $inputfolder\/$orgn\.fna`;
 `java -Xmx4096m -jar $pf\/picard.jar CreateSequenceDictionary R=$inputfolder\/$orgn\.fna O=$inputfolder\/$orgn.dict`;
 `bowtie2-build  $inputfolder\/$orgn\.fna $inputfolder\/$orgn\.fna`;
 `bwa index $inputfolder\/$orgn\.fna $inputfolder\/$orgn\.fna`;
-#makeblastdb -in test.fsa -dbtype nucl
-
+`dragen-os --build-hash-table true --ht-reference $inputfolder\/$orgn\.fna --output-directory $OUTSNAP`;	
 }
 1;
