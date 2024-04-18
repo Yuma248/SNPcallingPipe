de4a96228c51d92d44b235d03f4f7f708866ea55
Yuma <yuma248@gmail.com>
2024-04-18 11:29:36 +0930

    Update RemAdap.pm

--- SNPcallPipe/RemAdap.pm
+++ SNPcallPipe/RemAdap.pm
@@ -31,38 +31,52 @@ if (not defined ($exf)){$exf = "1.fq.gz,2.fq.gz";}
 if (not defined ($rbar)){$rbar = "n";}
 if ($rbar eq "y" && not defined ($barcode_file)){print "\nThis script will trim quality and collapse overlapping PE reads from several samples using AdapterRemoval in parallel. Requires your files compressed (sample01.1.fq.gz or sample01_1.fq.gz), all save in one folder, or each sample in a folders and all the folder per sample in one parental folder.\nIt has the option of remove barcodes if they were nor removed during demultiplexing step, for that it will require also a tab delimited barcodefile, with the first column the name of the sample (as in the fastq file) and second column the barcode sequence, if you want to remove also the RADtag, you can add it in the barcode sequence.\n\nUsages:\nSNPcallPipe.pl -stp trim\n\t-i <path to inputfolder, if samples are each in one folder use the option -fm>\n\nOptional:\n\t-o <path to outputfolder, default same as inputfolder>\n\t-snc <number the cores or samples to use in parallel, default 4>\n\t-fm <if sequence files are in one folder per sample (y or n), default n, either the folders or the sequences should be in one parental folder>\n\t-exf <Extension format of sequences fastq files, default 1.fq.gz,2.fq.gz>\n\t-rbar <If barcodes were not removed from sequences during demultiplexing step and you want to remove them, default not (n)>\n\t-bf <Patht to barcode file, it is required if you want to remove barcodes>\n\nExample:\nMELFUwgrs.pl -stp trim -i /yuma/WGS/ -o /yuma/remadap/ -fm y -snc 62 -exf F.fq.gz,R.fq.gz -rbar y -bf /path/to/barcodefile\n\n";exit;}
 if ($fm eq "n"){
-#my @names=`ls $inf/*1.fq.gz | sed 's/_1.fq.gz//g' | sed 's/.1.fq.gz//g'`;
-my @cod=split /\,/, $exf;
-#my $test=scalar @cod; 
-#print "$cod[0]\t$cod[1]\tYUMA\t$test)\n";
-my @names=`ls $inf/*$cod[0] | sed 's/$cod[0]//g'`;
-if ($rbar eq "n"){
-foreach $name (@names){chomp $name; $name=~ s/$inf\///g; push (@nms, $name);}
-if ((scalar (@cod)) < 2){
-my $cmd="parallel -j $nc --results $tmpdir --tmpdir $tmpdir AdapterRemoval --file1 $inf/{1}$cod[0] --basename $outf/{1} --minlength 30 --trimns --gzip ::: @nms";
-print "$cmd\n\n";
-`parallel -j $nc --results $tmpdir --tmpdir $tmpdir AdapterRemoval --file1 $inf/{1}$cod[0] --basename $outf/{1} --minlength 70 --trimns --trimqualities --collapse --gzip ::: @nms`;
-}
-elsif ((scalar (@cod)) == 2){
-my $cmd="`parallel -j $nc --results $tmpdir --tmp $tmpdir AdapterRemoval --file1 $inf/{1}$cod[0] --file2 $inf/{1}$cod[1] --basename $outf/{1} --minlength 70 --trimns --trimqualities --collapse --gzip ::: @nms";
-print "$cmd\n\n";
-`parallel -j $nc --results $tmpdir --tmpdir $tmpdir AdapterRemoval --file1 $inf/{1}$cod[0] --file2 $inf/{1}$cod[1] --basename $outf/{1} --minlength 70 --trimns --trimqualities --collapse --gzip ::: @nms`;
-}
-}
+	#my @names=`ls $inf/*1.fq.gz | sed 's/_1.fq.gz//g' | sed 's/.1.fq.gz//g'`;
+	my @cod=split /\,/, $exf;
+	#my $test=scalar @cod; 
+	#print "$cod[0]\t$cod[1]\tYUMA\t$test)\n";
+	my @names=`ls $inf/*$cod[0] | sed 's/$cod[0]//g'`;
+	if ($rbar eq "n"){
+		foreach $name (@names){chomp $name; $name=~ s/$inf\///g; push (@nms, $name);}
+			if ((scalar (@cod)) < 2){
+				my $cmd="parallel -j $nc --results $tmpdir --tmpdir $tmpdir AdapterRemoval --file1 $inf/{1}$cod[0] --basename $outf/{1} --minlength 30 --trimns --gzip ::: @nms";
+				print "$cmd\n\n";
+				`parallel -j $nc --results $tmpdir --tmpdir $tmpdir AdapterRemoval --file1 $inf/{1}$cod[0] --basename $outf/{1} --minlength 70 --trimns --trimqualities --collapse --gzip ::: @nms`;
+			}
+			elsif ((scalar (@cod)) == 2){
+				my $cmd="`parallel -j $nc --results $tmpdir --tmp $tmpdir AdapterRemoval --file1 $inf/{1}$cod[0] --file2 $inf/{1}$cod[1] --basename $outf/{1} --minlength 70 --trimns --trimqualities --collapse --gzip ::: @nms";
+				print "$cmd\n\n";
+				`parallel -j $nc --results $tmpdir --tmpdir $tmpdir AdapterRemoval --file1 $inf/{1}$cod[0] --file2 $inf/{1}$cod[1] --basename $outf/{1} --minlength 70 --trimns --trimqualities --collapse --gzip ::: @nms`;
+			}
+	}
+	else{
+	
+		foreach $name (@names){chomp $name; $name=~ s/$inf\///g; push (@nms, $name); $nBP=`grep $name $bf | cut -f 2 | wc -c`; push (@tbp5, $nBP);}
+			if ((scalar (@cod)) < 2){
+				my $cmd="parallel -j $nc --results $tmpdir --tmpdir $tmpdir AdapterRemoval --file1 $inf/{1}$cod[0] --basename $outf/{1} --minlength 30 --trimns --gzip ::: @nms";
+				print "$cmd\n\n";
+				`parallel -j $nc --results $tmpdir --tmpdir $tmpdir --link AdapterRemoval --file1 $inf/{1}$cod[0] --basename $outf/{1} --minlength 70 --trim5p {2} --trimns --trimqualities --collapse --gzip ::: @nms::: @tbp `;
+			}
+			elsif ((scalar (@cod)) == 2){
+				my $cmd="`parallel -j $nc --results $tmpdir --tmp $tmpdir AdapterRemoval --file1 $inf/{1}$cod[0] --file2 $inf/{1}$cod[1] --basename $outf/{1} --minlength 70 --trimns --trimqualities --collapse --gzip ::: @nms";
+				print "$cmd\n\n";
+				`parallel -j $nc --results $tmpdir --tmpdir $tmpdir --link AdapterRemoval --file1 $inf/{1}$cod[0] --file2 $inf/{1}$cod[1] --basename $outf/{1} --minlength 70 --trim5p {2} --trimns --trimqualities --collapse --gzip ::: @nms ::: @tbp`;
+			}
+	}
 
 }
 else {
-my @names=`ls -d $inf/*/`;
+	my @names=`ls -d $inf/*/`;
 
-foreach $name (@names){chomp $name; $name=~ s/$inf\///g; $name=~ s/\///g; push (@nms, $name);  
-	my $filesN = `ls $inf/$name/* | wc -l`;
-        if ($filesN <= 3){`mv $inf/$name/$name\_*_1.fq.gz $inf/$name/$name\_1.fq.gz`; `mv $inf/$name/$name\_*_2.fq.gz $inf/$name/$name\_2.fq.gz`;}
-	else {`cat $inf\/$name\/$name\_\*_1.fq.gz \>\> $inf\/$name\/$name\_1.fq.gz`; `cat $inf\/$name\/$name\_\*_2.fq.gz \>\> $inf\/$name\/$name\_2.fq.gz`;}
-}
+	foreach $name (@names){chomp $name; $name=~ s/$inf\///g; $name=~ s/\///g; push (@nms, $name);  
+		my $filesN = `ls $inf/$name/* | wc -l`;
+		if ($filesN <= 3){`mv $inf/$name/$name\_*_1.fq.gz $inf/$name/$name\_1.fq.gz`; `mv $inf/$name/$name\_*_2.fq.gz $inf/$name/$name\_2.fq.gz`;}
+		else {`cat $inf\/$name\/$name\_\*_1.fq.gz \>\> $inf\/$name\/$name\_1.fq.gz`; `cat $inf\/$name\/$name\_\*_2.fq.gz \>\> $inf\/$name\/$name\_2.fq.gz`;}
+	}
 
-my $cmd="`parallel -j $nc --results $tmpdir --tmp $tmpdir AdapterRemoval --file1 $inf/{1}/{1}$cod[0] --file2 $inf/{1}$cod[1] --basename $outf/{1} --minlength 70 --trimns --trimqualities --collapse --gzip ::: @nms";
-print "$cmd\n\n";
-`parallel -j $nc --results $tmpdir --tmpdir $tmpdir AdapterRemoval --file1 $inf/{1}/{1}$cod[0] --file2 $inf/{1}/{1}$cod[1] --basename $outf/{1} --minlength 70 --trimns --trimqualities --collapse --gzip ::: @nms`;
+	my $cmd="`parallel -j $nc --results $tmpdir --tmp $tmpdir AdapterRemoval --file1 $inf/{1}/{1}$cod[0] --file2 $inf/{1}$cod[1] --basename $outf/{1} --minlength 70 --trimns --trimqualities --collapse --gzip ::: @nms";
+	print "$cmd\n\n";
+	`parallel -j $nc --results $tmpdir --tmpdir $tmpdir AdapterRemoval --file1 $inf/{1}/{1}$cod[0] --file2 $inf/{1}/{1}$cod[1] --basename $outf/{1} --minlength 70 --trimns --trimqualities --collapse --gzip ::: @nms`;
 }
 
 
